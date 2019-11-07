53e21bb7380b0c4f7ff0b9c7a723df846c3d42a1
jitsew <jitse.wierdsma@gmail.com>
2019-11-08 00:04:31 +0100

    update

--- app/src/main/java/com/example/PDM/adapters/BoardGameAdapter.kt
+++ app/src/main/java/com/example/PDM/adapters/BoardGameAdapter.kt
@@ -1,20 +1,28 @@
 package com.example.PDM.adapters
 
+import BoardGame
+import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.example.PDM.BoardGameActivity
 import com.example.PDM.R
 import com.example.PDM.dtos.GameDTO
+import com.squareup.picasso.Picasso
 import isel.leic.i1920.pdm.li51n.viewmodel.BoardGamesViewModel
+import kotlinx.android.synthetic.main.list_item.view.*
 
 class BoardGameAdapter(private val model : BoardGamesViewModel) : RecyclerView.Adapter<BoardGamesViewHolder>()
 {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BoardGamesViewHolder {
         val view = LayoutInflater.from(parent.context)
                 //TODO set the correct view of the window
-            .inflate(R.layout.activity_board_game, parent, false) as LinearLayout
-        return BoardGamesViewHolder(view)
+            .inflate(R.layout.list_item, parent, false)
+        return BoardGamesViewHolder(view, parent.context)
     }
 
     override fun getItemCount(): Int = model.games.value?.size ?: 0
@@ -25,12 +33,31 @@ class BoardGameAdapter(private val model : BoardGamesViewModel) : RecyclerView.A
 }
 
 //TODO adjust this class
-class BoardGamesViewHolder(private val view: LinearLayout) : RecyclerView.ViewHolder(view) {
+class BoardGamesViewHolder(private val view: View, val context: Context) : RecyclerView.ViewHolder(view) {
+    var currentBoardGame: GameDTO? = null
+
+    init {
+        itemView.setOnClickListener {
+            currentBoardGame?.let {
+                val intent = Intent(context, BoardGameActivity::class.java)
+                intent.putExtra("Board Game", currentBoardGame)
+                context.startActivity(intent)
+            }
+        }
+    }
+
     //TODO set the correct view attributes
 //    private val txtAlbumName: TextView = view.findViewById<TextView>(R.id.txtAlbumName)
 //    private val txtPlaycount: TextView = view.findViewById<TextView>(R.id.txtPlaycount)
 
     fun bindTo(game: GameDTO) {
         //TODO set binding
+        game.let {
+            itemView.txvTitle.text = game.name
+            Log.d("game", game.rating.toString())
+            itemView.txvRating.text = "%.1f".format(game.rating)
+            Picasso.get().load(game.image_url).into(itemView.imgThumbnail)
+        }
+        this.currentBoardGame = game
     }
 }
--- app/src/main/java/com/example/PDM/lastfm/dto/dto/GameDTO.kt
+++ app/src/main/java/com/example/PDM/lastfm/dto/dto/GameDTO.kt
@@ -1,5 +1,7 @@
 package com.example.PDM.dtos
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
@@ -14,4 +16,49 @@ class GameDTO(
     val desc: String?,
     val company: String?,
     val creators: Array<String?>,
-    val rating: Double?, val url: String?)
+    val rating: Double?, val url: String?) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readString(),
+        parcel.readString(),
+        parcel.createStringArray() as Array<String?>,
+        parcel.readValue(Double::class.java.classLoader) as? Double,
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(image_url)
+        parcel.writeString(name)
+        parcel.writeValue(year_published)
+        parcel.writeValue(min_players)
+        parcel.writeValue(max_players)
+        parcel.writeValue(min_age)
+        parcel.writeString(desc)
+        parcel.writeString(company)
+        parcel.writeStringArray(creators)
+        parcel.writeValue(rating)
+        parcel.writeString(url)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<GameDTO> {
+        override fun createFromParcel(parcel: Parcel): GameDTO {
+            return GameDTO(parcel)
+        }
+
+        override fun newArray(size: Int): Array<GameDTO?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
