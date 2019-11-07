db62398fff721cd11be5915e9c4a8ad963fa6ddb
jitsew <jitse.wierdsma@gmail.com>
2019-11-07 23:44:55 +0100

    Info button

--- app/src/main/java/com/example/PDM/MainActivity.kt
+++ app/src/main/java/com/example/PDM/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.PDM
 
 import BoardGameGeek
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -39,6 +40,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
          * Setup search button
          */
         findViewById<Button>(R.id.buttonSearch).setOnClickListener(this)
+
+        val infoButton = findViewById<Button>(R.id.infoButton)
+        infoButton.setOnClickListener {
+            val intent = Intent(this, InfoActivity::class.java)
+            this.startActivity(intent)
+        }
     }
 
     override fun onClick(v: View) {
@@ -56,6 +63,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 txtTotalBoardGames.text = games.size.toString()
             })
     }
+
+
 
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
